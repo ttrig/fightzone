ed0a63d721bd1893c98a1bd40965ec613f099dbd
markus <markus.lilienberg@glesys.se>
2021-02-26 21:47:06 +0100

    Quickfix

--- app/Http/Controllers/YouthKickboxingController.php
+++ app/Http/Controllers/YouthKickboxingController.php
@@ -4,11 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Repositories\PageRepository;
 
-class YouthBoxingController extends Controller
+class YouthKickboxingController extends Controller
 {
     public function __invoke(PageRepository $page)
     {
-        return view('youth_boxing', [
+        return view('youth_kickboxing', [
             'alerts' => $page->alerts(),
             'texts' => $page->texts(),
         ]);

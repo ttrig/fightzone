d9401863cc36eb6e0a4f9ebf16ccccac325bbfe3
markus <markus.lilienberg@glesys.se>
2023-05-29 15:54:43 +0200

    Update sponsors

--- resources/views/layouts/_footer_sponsors.blade.php
+++ resources/views/layouts/_footer_sponsors.blade.php
@@ -7,60 +7,9 @@
     </div>
     <div class="row my-3">
       <div class="col-md-12">
-        <a href="https://pampasreklam.se" target="_blank">
-          <img
-            class="lazy"
-            data-src="/images/sponsors/pampas_reklam.png"
-            alt="Pampas reklam"
-          ></a>
-        <a href="http://www.mabi.se" target="_blank">
-          <img
-            class="lazy"
-            data-src="/images/sponsors/mabi_hyrbilar.png"
-            alt="Mabi Hyrbilar"
-          ></a>
-        <a href="http://www.beve.se" target="_blank">
-          <img
-            class="lazy"
-            data-src="/images/sponsors/beve.png"
-            alt="Beve"
-          ></a>
         <a href="http://www.worx.nu" target="_blank">
-          <img
-            class="lazy"
-            data-src="/images/sponsors/worx.png"
-            alt="Worx"
-          ></a>
-        <a href="https://www.kakelmiljoskane.se" target="_blank">
-          <img
-            class="lazy"
-            data-src="/images/sponsors/kakelmiljo_i_skane.png"
-            alt="Kakelmilj&ouml; i Sk&aring;ne"
-          ></a>
-        <a href="https://www.apexrehab.se" target="_blank">
-          <img
-            class="lazy"
-            data-src="/images/sponsors/apex_rehab.png"
-            alt="Apex Rehab"
-          ></a>
-        <a href="https://www.baltzartandvard.se" target="_blank">
-          <img
-            class="lazy"
-            data-src="/images/sponsors/baltzartandvard.png"
-            alt="Baltzar Tandv&aring;rd"
-          ></a>
-        <a href="https://www.unitedgross.com" target="_blank">
-          <img
-            class="lazy"
-            data-src="/images/sponsors/united_gross.png"
-            alt="United Gross"
-          ></a>
-        <a href="https://www.folketsbygg.com" target="_blank">
-          <img
-            class="lazy"
-            data-src="/images/sponsors/folkets_bygg.png"
-            alt="United Gross"
-          ></a>
+          <img class="lazy" data-src="/images/sponsors/worx.png" alt="Worx">
+        </a>
       </div>
     </div>
   </div>
